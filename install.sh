1d0d30b34062888d99082f67528b28765dbd9644
Dave Eddy <dave@daveeddy.com>
2012-06-18 16:29:43 -0700

    SunOS doesn't support -v with `ln`

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 #
 # install all files to ~ by symlinking them,
 # this way, updating them is as simple as git pull
+set -x
 for f in *; do
-	ln -svf "$PWD/$f" ~/."$f"
+	ln -sf "$PWD/$f" ~/."$f"
 done
+set +x
