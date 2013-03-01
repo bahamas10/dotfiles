f67ae236ced74b698daa7ed6cb55d71175ae4fd2
Dave Eddy <dave@daveeddy.com>
2013-02-28 23:50:24 -0800

    adding default keybindings

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 # this way, updating them is as simple as git pull
 
 for f in bash_profile bashrc htoprc jshintrc screenrc tmux.conf vimrc; do
-	printf '%-50s -> %s\n' "$PWD/$f" "$HOME/.$f"
+	printf '%40s -> %s\n' "$PWD/$f" "$HOME/.$f"
 	ln -sf "$PWD/$f" ~/."$f"
 done
+
+if [[ -d ~/Library ]]; then
+	echo 'Installing KeyBindings for MacOS'
+	mkdir -p ~/Library/KeyBindings
+	cp DefaultKeyBinding.dict ~/Library/KeyBindings
+fi
