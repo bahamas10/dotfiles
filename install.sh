81644fc5bcd6c3f7f2fb0fefdd41cb987d893e6d
Dave Eddy <dave@daveeddy.com>
2013-08-24 18:13:47 -0400

    add nsuserdefaults customization

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # install all files to ~ by symlinking them,
 # this way, updating them is as simple as git pull
 
+defaults() {
+	echo "$@"
+	command defaults "$@"
+}
+
 for f in bash_profile bashrc htoprc jshintrc screenrc tmux.conf vimrc; do
 	printf '%40s -> %s\n' "$PWD/$f" "$HOME/.$f"
 	ln -sf "$PWD/$f" ~/."$f"
@@ -14,4 +19,9 @@ if [[ -d ~/Library ]]; then
 	f='DefaultKeyBinding.dict'
 	printf '%40s -> %s\n' "$PWD/$f" ~/Library/KeyBindings/"$f"
 	ln -sf "$PWD/$f" ~/Library/KeyBindings/"$f"
+fi
+
+if defaults read com.apple.finder &>/dev/null; then
+	defaults write com.apple.finder _FXShowPosixPathInTitle Yes
+	defaults write com.apple.dashboard mcx-disabled -boolean YES
 fi
