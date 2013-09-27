ddaf42089f83f6d39a685ada5541efe164af4c12
Dave Eddy <dave@daveeddy.com>
2013-09-27 16:50:05 -0400

    comment out update for now

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ symlink() {
 
 git submodule init
 git submodule update
-git submodule foreach git pull origin master
+#git submodule foreach git pull origin master
 
 # Link dotfiles
 for f in bash_profile bashrc htoprc jshintrc screenrc tmux.conf vimrc vim; do
