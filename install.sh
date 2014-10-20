96ce590afc7d753687b7f8252e31e1cb16e5c683
Dave Eddy <dave@daveeddy.com>
2014-10-20 01:07:02 -0400

    whoops

--- install.sh
+++ install.sh
@@ -175,24 +175,6 @@ if defaults read com.apple.finder &>/dev/null; then
 
 	### Spotlight
 
-	defaults write com.apple.spotlight orderedItems -array \
-		'{"enabled" = 1;"name" = "APPLICATIONS";}' \
-		'{"enabled" = 0;"name" = "SYSTEM_PREFS";}' \
-		'{"enabled" = 0;"name" = "DIRECTORIES";}' \
-		'{"enabled" = 0;"name" = "PDF";}' \
-		'{"enabled" = 0;"name" = "FONTS";}' \
-		'{"enabled" = 0;"name" = "DOCUMENTS";}' \
-		'{"enabled" = 0;"name" = "MESSAGES";}' \
-		'{"enabled" = 0;"name" = "CONTACT";}' \
-		'{"enabled" = 0;"name" = "EVENT_TODO";}' \
-		'{"enabled" = 0;"name" = "IMAGES";}' \
-		'{"enabled" = 0;"name" = "BOOKMARKS";}' \
-		'{"enabled" = 0;"name" = "MUSIC";}' \
-		'{"enabled" = 0;"name" = "MOVIES";}' \
-		'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-		'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-		'{"enabled" = 0;"name" = "SOURCE";}'
-
 	# Don't index mounted volumes
 	sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array /Volumes
 
